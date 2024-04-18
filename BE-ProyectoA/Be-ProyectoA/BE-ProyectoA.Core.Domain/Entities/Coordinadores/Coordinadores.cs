867adfe1d01c22dfebc2465fd825df4cd56647f6
Rainiery <rainierypenajrg@gmail.com>
2024-04-18 17:26:51 -0400

    creando constructor de la entidad Coordinador

--- BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Core.Domain/Entities/Coordinadores/Coordinadores.cs
+++ BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Core.Domain/Entities/Coordinadores/Coordinadores.cs
@@ -7,11 +7,30 @@ namespace BE_ProyectoA.Core.Domain.Entities.Coordinador
     public sealed class Coordinadores : AggregateRoot  
     {
 
+
+        //constructor principal
+        public Coordinadores() { }
+        //contructor con parametros para quitar los nulo y 
+        //usar el metodo actualizar
+        public Coordinadores(CoordinadoresId id,string nombre, string apellido, int cantidadVotos, NumeroTelefono numeroTelefono, Cedula cedula, bool activo)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+            Cedula = cedula;
+            CantidadVotos = cantidadVotos;
+            NumeroTelefono = numeroTelefono;
+            Activo = activo;
+            
+
+        }
         public CoordinadoresId Id { get; set; }
         public string Nombre { get; set; }  = string.Empty;
         public string Apellido { get; set; } = string.Empty;
+        public string NombreCompleto => $"{Nombre} {Apellido}";
         public int CantidadVotos { get; set; } 
-        public Cedula Cedula { get; private set; }  
+        public Cedula Cedula { get; private set; } 
+        public bool Activo {  get; private set; }   
         public NumeroTelefono NumeroTelefono { get; private set; }
     }
 }
