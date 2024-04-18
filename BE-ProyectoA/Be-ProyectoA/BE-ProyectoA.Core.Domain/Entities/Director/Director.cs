8c4b6da5e312230be5d51f39d440d3b1796051e5
Rainiery <rainierypenajrg@gmail.com>
2024-04-18 17:35:43 -0400

    Creando constructor para la entidad Director

--- BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Core.Domain/Entities/Director/Director.cs
+++ BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Core.Domain/Entities/Director/Director.cs
@@ -5,13 +5,30 @@ namespace BE_ProyectoA.Core.Domain.Entities.Director
 {
     public sealed class Director : AggregateRoot
     {
+
+        public Director() { }
+
+        public Director(DirectorId id, string nombre, string apellido, int cantidadVotantes, Cedula cedula, NumeroTelefono numeroTelefono, bool activo)
+        {
+            Id = id;
+            NumeroTelefono = numeroTelefono;
+            Nombre = nombre;
+            Apellido = apellido;
+            Cedula = cedula;
+            CantidadVotantes = cantidadVotantes;
+            Activo = activo;
+
+
+        
+        }
         public DirectorId Id { get; set; }
         public string Nombre { get;  private set; } = string.Empty;
         public string Apellido { get; private set; } = string.Empty;
         public string NombreCompleto => $"{Nombre} {Apellido}";
-        public int CantidadVotos { get; set; }  
+        public int CantidadVotantes { get; set; }  
         public Cedula Cedula { get;  private  set; } 
         public NumeroTelefono NumeroTelefono { get; private set; }
+        public bool Activo { get; set; }
      
     }
 }
