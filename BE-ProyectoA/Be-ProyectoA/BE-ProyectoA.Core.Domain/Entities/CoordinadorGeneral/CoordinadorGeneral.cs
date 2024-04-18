6485e510335f3eff26b7c397d3e267cda7c7aba6
Rainiery <rainierypenajrg@gmail.com>
2024-04-18 18:07:10 -0400

    Creacion de entidad Coordinador General

--- BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Core.Domain/Entities/CoordinadorGeneral/CoordinadorGeneral.cs
+++ BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Core.Domain/Entities/CoordinadorGeneral/CoordinadorGeneral.cs
@@ -1,9 +1,34 @@
 ﻿using BE.MovieApp.Core.Domain.Primitivies;
+using BE_ProyectoA.Core.Domain.ValueObjects;
+
 
 namespace BE_ProyectoA.Core.Domain.Entities.CoordinadorGeneral
 {
     public class CoordinadorGeneral  : AggregateRoot
     {
+
        public CoordinadorGeneral() { }
+       public CoordinadorGeneral(CoordinadorGeneralId id, string nombre,string apellido, Cedula cedula, NumeroTelefono numeroTelefono, bool activo,Direccion direccion)    
+        { 
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+            Cedula = cedula;
+            NumeroTelefono = numeroTelefono;
+            Activo = activo;
+            Direccion = direccion;
+
+        }
+
+
+        public CoordinadorGeneralId Id { get; set; }  
+        public string Nombre { get; set; }  = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string NombreCompleto => $"{Nombre} {Apellido}";
+        public Cedula Cedula { get; set; }
+        public NumeroTelefono NumeroTelefono { get; set; } 
+        public Direccion Direccion { get; set; }
+        public bool Activo { get; set; }
+
     }
 }
