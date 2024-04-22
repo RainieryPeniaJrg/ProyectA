3afa662dc7f797daebfcc0c1017cb833e70c61da
Rainiery <Rainierypenajrg@gmail.com>
2024-04-22 17:16:50 -0400

    Limpiando Creando clase AplicationDbContext

--- BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Infraestructure.Persistence/DependencyInjection.cs
+++ BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Infraestructure.Persistence/DependencyInjection.cs
@@ -1,15 +1,9 @@
-﻿using BE.MovieApp.Core.Application.Data;
-using BE.MovieApp.Core.Domain.Movies;
-using BE.MovieApp.Core.Domain.Primitivies;
+﻿using BE.MovieApp.Core.Domain.Primitivies;
 using BE.MovieApp.Infraestructure.Persistence.persistence;
-using BE.MovieApp.Infraestructure.Persistence.persistence.Repositories;
+using BE_ProyectoA.Core.Application.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using BE.MovieApp.Core.Domain.Actors;
-using BE.MovieApp.Core.Domain.ActorCharacter;
-using BE.MovieApp.Core.Domain.Comments;
-using BE.MovieApp.Core.Domain.MovieCast;
 namespace BE.MovieApp.Infraestructure.Persistence
 {
     public static class DependencyInjection
@@ -33,11 +27,6 @@ namespace BE.MovieApp.Infraestructure.Persistence
 
             services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
 
-            services.AddScoped<IMovieRepository, MovieRepository>();
-            services.AddScoped<IActorRepository, ActorRepository>();
-            services.AddScoped<IActorCharacterRepository, ActorCharacterRepository>();
-            services.AddScoped<ICommentRepository, CommentRepository>();
-            services.AddScoped<IMovieCastRepository, MovieCastRepository>();
 
             return services;
         }
