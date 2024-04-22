d7fa4c588869fa4b9437eda33ec48f4c39c12d77
Rainiery <Rainierypenajrg@gmail.com>
2024-04-22 19:24:57 -0400

    Creando clases comunes para el contexto de la api

---
+++ BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Presentation.WebApi/Common/Http/HttpContextItemKeys.cs
@@ -0,0 +1,7 @@
+﻿namespace BE_ProyectoA.Presentation.WebApi.Common.Http
+{
+    public static class HttpContextItemKeys
+    {
+        public const string Errors = "errors";
+    }
+}
---
+++ BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Presentation.WebApi/Controllers/ApiController.cs
@@ -0,0 +1,49 @@
+﻿using ErrorOr;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BE_ProyectoA.Presentation.WebApi.Controllers
+{
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        protected IActionResult Problem(List<Error> errors)
+        {
+            if (errors.Count is 0)
+            {
+                return Problem();
+            }
+
+            if (errors.All(error => error.Type == ErrorType.Validation))
+            {
+                return ValidationProblem(errors);
+            }
+            HttpContext.Items[HttpContextItemKeys.Errors] = errors;
+
+            return Problem(errors[0]);
+        }
+
+        private IActionResult Problem(Error error)
+        {
+            var statusCode = error.Type switch
+            {
+                ErrorType.Conflict => StatusCodes.Status409Conflict,
+                ErrorType.Validation => StatusCodes.Status400BadRequest,
+                ErrorType.NotFound => StatusCodes.Status404NotFound,
+                _ => StatusCodes.Status500InternalServerError,
+            };
+            return Problem(statusCode: statusCode, title: error.Description);
+        }
+
+        private IActionResult ValidationProblem(List<Error> error)
+        {
+            var modelStateDictionary = new ModelStateDictionary();
+            foreach (var errorItem in error)
+            {
+                modelStateDictionary.AddModelError(errorItem.Code, errorItem.Description);
+            }
+
+            return ValidationProblem(modelStateDictionary);
+        }
+    }
+}
