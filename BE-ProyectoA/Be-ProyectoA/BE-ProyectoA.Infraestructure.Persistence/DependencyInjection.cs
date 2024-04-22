f9ebe0731f2e57c9361e7abf5e7b87f4458fb4b4
Rainiery <Rainierypenajrg@gmail.com>
2024-04-22 17:14:02 -0400

    first commit Persisntece

---
+++ BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Infraestructure.Persistence/DependencyInjection.cs
@@ -0,0 +1,45 @@
+﻿using BE.MovieApp.Core.Application.Data;
+using BE.MovieApp.Core.Domain.Movies;
+using BE.MovieApp.Core.Domain.Primitivies;
+using BE.MovieApp.Infraestructure.Persistence.persistence;
+using BE.MovieApp.Infraestructure.Persistence.persistence.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using BE.MovieApp.Core.Domain.Actors;
+using BE.MovieApp.Core.Domain.ActorCharacter;
+using BE.MovieApp.Core.Domain.Comments;
+using BE.MovieApp.Core.Domain.MovieCast;
+namespace BE.MovieApp.Infraestructure.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfraEstructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddPersistence(configuration);
+            return services;
+        }
+
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+                
+            services.AddDbContext<ApplicationDbContext>
+                (
+                         options =>
+                         options.UseSqlServer(configuration.GetConnectionString("Database")));
+
+            services.AddScoped<IApplicationDbContext>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+            services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+            services.AddScoped<IMovieRepository, MovieRepository>();
+            services.AddScoped<IActorRepository, ActorRepository>();
+            services.AddScoped<IActorCharacterRepository, ActorCharacterRepository>();
+            services.AddScoped<ICommentRepository, CommentRepository>();
+            services.AddScoped<IMovieCastRepository, MovieCastRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Infraestructure.Persistence/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,57 @@
+﻿using BE.MovieApp.Core.Application.Data;
+using BE.MovieApp.Core.Domain.ActorCharacter;
+using BE.MovieApp.Core.Domain.Actors;
+using BE.MovieApp.Core.Domain.Comments;
+using BE.MovieApp.Core.Domain.MovieCast;
+using BE.MovieApp.Core.Domain.Movies;
+using BE.MovieApp.Core.Domain.Primitivies;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BE.MovieApp.Infraestructure.Persistence.persistence
+{
+    public class ApplicationDbContext : DbContext,IApplicationDbContext,IUnitOfWork
+    {
+        private readonly IPublisher _publisher;
+
+        public ApplicationDbContext(IPublisher publisher, DbContextOptions options): base(options)
+        {
+            _publisher = publisher ?? throw new ArgumentNullException(nameof(publisher));
+          
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies(true).LogTo(s => System.Console.WriteLine(s));
+
+           
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+        public DbSet<Movies> Movies { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Comments> Comments { get; set; }
+        public DbSet<ActorCharacter> ActorCharacters { get; set; }
+        public DbSet<MovieCast> MoviesCasts { get; set; }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            var events = ChangeTracker.Entries<AggregateRoot>()
+                .Select(e => e.Entity)
+                .Where(e => e.GetDomainEvents().Any())
+                .SelectMany(e => e.GetDomainEvents());
+
+            var result = await base.SaveChangesAsync(cancellationToken);
+
+            foreach (var domainEvents in events)
+            {
+                await _publisher.Publish(domainEvents, cancellationToken);
+            }
+
+            return result;
+
+        }
+    }
+}
