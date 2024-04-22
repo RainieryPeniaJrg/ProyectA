6e1cc51d3ef20c5e5f02b10c26e0717cf4bfafde
Rainiery <Rainierypenajrg@gmail.com>
2024-04-22 19:28:44 -0400

    Creando controlador de errores de la api

---
+++ BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Presentation.WebApi/Controllers/ErrorsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BE_ProyectoA.Presentation.WebApi.Controllers
+{
+    public class ErrorsController : ControllerBase
+    {
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [Route("/error")]
+
+        public IActionResult Error()
+        {
+            Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+            return Problem();
+        }
+    }
+}
