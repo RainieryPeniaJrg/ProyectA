2afc946435ce1ad4f294e2e877a998ae1d9ad3d9
Rainiery <Rainierypenajrg@gmail.com>
2024-04-22 17:06:08 -0400

    creando scope de indentity en program

--- BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Presentation.WebApi/Program.cs
+++ BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Presentation.WebApi/Program.cs
@@ -1,3 +1,7 @@
+using BE_ProyectoA.Persistence.Identity.Model;
+using BE_ProyectoA.Persistence.Identity.Seeds;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +13,28 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+        await DefaultRoles.SeedAsync(userManager, roleManager);
+        await DefaultAdminUser.SeedAsync(userManager, roleManager);
+        await DefaultDirigenteUser.SeedAsync(userManager, roleManager);
+        await DefaultCoordinadorUser.SeedAsync(userManager, roleManager);
+        await DefaultSubCoordinador.SeedAsync(userManager, roleManager);
+    }
+    catch (Exception ex)
+    {
+        throw;
+    }
+    app.Run();
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
