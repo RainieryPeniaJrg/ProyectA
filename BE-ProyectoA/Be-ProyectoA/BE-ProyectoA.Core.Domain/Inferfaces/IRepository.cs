fb688dc441d301b37870f5d19aac8cf3f572b9f7
Rainiery <Rainierypenajrg@gmail.com>
2024-04-22 23:15:26 -0400

    reset identity

---
+++ BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Core.Domain/Inferfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace Be_NetBanking.Core.Domain.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity?> GetEntityAsync(Expression<Func<TEntity, bool>>? filter = null, bool tracked = true);
+        Task<List<TEntity>> GetEntitiesAsync(Expression<Func<TEntity, bool>>? filter = null);
+        Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> filter);
+        Task SaveAsync(TEntity entity);
+        Task SaveAsync(TEntity[] entities);
+        Task UpdateAsync(TEntity entity);
+        Task UpdateAsync(TEntity[] entities);
+        Task RemoveAsync(TEntity entity);
+        Task RemoveAsync(TEntity[] entities);
+        Task SaveChangesAsync();
+
+    }
+}
