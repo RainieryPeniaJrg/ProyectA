043073c968f96cad53c6fb3860508556a595d7f7
Rainiery <Rainierypenajrg@gmail.com>
2024-04-23 23:17:57 -0400

    Creando Command grupos

---
+++ BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Core.Application/GrupoFeatures/Commands/Create/CreateGruposCommand.cs
@@ -0,0 +1,27 @@
+﻿using BE_ProyectoA.Core.Domain.Entities.Coordinadores;
+using BE_ProyectoA.Core.Domain.Entities.CoordinadorGeneral;
+using BE_ProyectoA.Core.Domain.Entities.DirigenteMultiplicador;
+using BE_ProyectoA.Core.Domain.Entities.GruposEntity;
+using ErrorOr;
+using MediatR;
+
+namespace BE_ProyectoA.Core.Application.GrupoFeatures.Commands.Create
+{
+    public record CreateGruposCommand
+ 
+        (
+          Guid Id,
+         string NombreGrupo,
+         Guid DirigentesMultiplicadoresId,
+
+         Guid SubCoordinadoresId,
+ 
+        Guid CoordinadoresGeneralesId,
+    
+
+     bool Active
+        
+        )
+        : IRequest<ErrorOr<Unit>>;
+   
+}
