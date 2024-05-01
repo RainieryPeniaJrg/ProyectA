9aa6c1fc3f2065e0f1be70a3736eef6f8ac78f80
Rainiery <Rainierypenajrg@gmail.com>
2024-05-01 15:36:16 -0400

    permiento nulabilidad en campos direccion

---
+++ BE-ProyectoA/Be-ProyectoA/BE-ProyectoA.Core.Application/VotantesFeatures/VotantesCoordinador/Common/VotantesCoordinadorResponse.cs
@@ -0,0 +1,31 @@
+﻿using BE_ProyectoA.Core.Domain.ValueObjects;
+
+namespace BE_ProyectoA.Core.Application.VotantesFeatures.VotantesCoordinador.Common
+{
+    public record VotantesCoordinadorResponse
+        (
+        
+        
+        );
+
+    public record CoordinadorGeneralResponse(
+       string Nombre,
+       string Apellido
+   );
+    public record VotantesResponse(
+        Guid Id,
+        string NombreCompleto,
+        Cedula Cedula,
+        NumeroTelefono NumeroTelefono,
+        DireccionResponse Direccion,
+        bool Activo,
+        CoordinadorGeneralResponse? CoordinadorGeneral
+   
+        );
+
+    public record DireccionResponse(
+     string Provincia,
+     string Sector
+ );
+
+}
